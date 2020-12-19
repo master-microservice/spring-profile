5aecac548564eeef89850d36c369cd1e8b01b064
Nitesh Nandan <nitesh.na@imimobile.com>
2020-12-19 11:09:01 +0530

    intial commit

---
+++ src/main/java/mis/profile/springprofile/controller/Home.java
@@ -0,0 +1,22 @@
+package mis.profile.springprofile.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Spring profiling";
+    }
+    @GetMapping({"/profile"})
+    public String[] checkProfile() {
+        return environment.getActiveProfiles();
+    }
+}
